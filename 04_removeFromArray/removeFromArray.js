4b07107db7ec0826c5f2acac7c84d6e26beb28d4
guitarmachintos <guitarmachintos@gmail.com>
2025-04-04 00:00:10 +0700

    finish ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, toRemove) {
+    if (!Array.isArray(arguments[0])){
+        return "ERROR";
+    }
+
+    for (let i = 1; i < arguments.length; i++) {
+        let j = 0;
+        while (j < arr.length){
+            if(arr[j] === arguments[i]){
+                arr.splice(j,1);
+            }
+            else{
+                j++;
+            }
+        }
+        // for (let j = 0; j < arr.length; j++) {
+        //     if(arr[j] === arguments[i]){
+        //         arr.splice(j,1);
+        //     }            
+        // }       
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
