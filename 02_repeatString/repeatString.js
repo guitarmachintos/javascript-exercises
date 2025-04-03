595a543a2535c4725f97dac2fb61605dd3fb4507
guitarmachintos <guitarmachintos@gmail.com>
2025-04-03 20:19:42 +0700

    finish ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, nRepeat) {
+    finalString = '';
+    if (nRepeat < 0){
+        finalString += "ERROR";
+    }
+    else{
+        for (let i = 0; i < nRepeat; i++) {
+            finalString += text;            
+        }
+    }
+    return finalString;
 };
 
 // Do not edit below this line
