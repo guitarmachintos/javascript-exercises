dadbe588a72a7364103850921c10c799c012c651
guitarmachintos <guitarmachintos@gmail.com>
2025-04-03 20:51:36 +0700

    finish ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversedString = '';
+    if(text.length < 0){
+        reversedString = 'ERROR';
+    }
+    else{
+        for (let i = text.length-1; i >= 0; i--) {
+            reversedString += text[i];            
+        }
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
