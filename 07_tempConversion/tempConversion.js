402a0cadce87edd53cbf7962590e4bc2e4a4dd9a
guitarmachintos <guitarmachintos@gmail.com>
2025-04-04 23:32:59 +0700

    finish ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function round2Dec(n) {
+  return Math.round(n * 10) / 10;
+}
+
+const convertToCelsius = function(fTemp) {
+  return round2Dec((fTemp - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  return round2Dec((cTemp * 9 / 5) + 32);
 };
 
 // Do not edit below this line
