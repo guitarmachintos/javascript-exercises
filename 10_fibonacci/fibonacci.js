504799d7b34eb3179fb5353fbb2bbba60276f263
guitarmachintos <50170621+guitarmachintos@users.noreply.github.com>
2025-04-25 21:20:31 +0700

    finish ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,49 @@
-const fibonacci = function() {
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
 
+const fibonacci = function(n) {
+    let a = 1;
+    let b = 1;
+
+    if(isNumeric(n) || typeof n == 'number'){
+        n = +n;
+    }
+    else{
+        return 'OOPS';
+    }
+
+    if(n<0){
+        return 'OOPS';
+    }
+    else if(n == 0){
+        return 0;
+    }
+    else if(n == 1 || n == 2){
+        return 1;
+    }
+    else{
+        let curCount = 2;
+        let aUpdating = true;
+        while (curCount < n){
+            if(aUpdating){
+                a = a + b;
+                aUpdating = !aUpdating;
+            }
+            else{
+                b = a + b;
+                aUpdating = !aUpdating;
+            }
+            curCount++;
+        }
+        return (n%2) ? a : b; 
+    }
 };
+
+console.log(fibonacci(4));
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
