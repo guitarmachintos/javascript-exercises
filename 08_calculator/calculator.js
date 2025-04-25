3b113d4e5f9ce2511618a06648bd80935808b81c
guitarmachintos <50170621+guitarmachintos@users.noreply.github.com>
2025-04-25 19:15:44 +0700

    finish ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, cur) => sum + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, cur) => sum * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factResult = 1;
+  for (let i = 1; i <= a; i++) {
+    factResult *= i;    
+  }
+  return factResult;
 };
 
 // Do not edit below this line
