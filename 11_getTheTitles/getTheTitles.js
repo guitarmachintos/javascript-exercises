7a871e4f695a1b596dedbc1b072eea272c90a627
guitarmachintos <50170621+guitarmachintos@users.noreply.github.com>
2025-04-26 18:52:07 +0700

    finish all ex

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookO) {
+    let titles = [];
+    for (const book of bookO) {
+        titles.push(book.title);        
+    }
+    return titles;
 };
+
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,52 @@
-const findTheOldest = function() {
+function fixDeath(people) {
+    return people.map(e=>{
+        if(!Object.hasOwn(e, 'yearOfDeath')){
+            let curYear = new Date();
+            curYear = curYear.getFullYear();
+            e.yearOfDeath = curYear;
+            return e;
+        }
+        return e;
+    })    
+}
 
+const findTheOldest = function(people) {
+    if(people.length == 0) return 'error';
+
+    people = fixDeath(people);
+
+    return people.reduce((finalPerson, curPerson) => {
+        let finalPersonAge = finalPerson.yearOfDeath - finalPerson.yearOfBirth;
+        let curPersonAge = curPerson.yearOfDeath - curPerson.yearOfBirth;
+        if(isNaN(finalPersonAge)){
+            return curPerson;
+        }
+        if(isNaN(curPersonAge)){
+            return finalPerson;
+        }
+        return finalPersonAge > curPersonAge ? finalPerson : curPerson;
+    })
 };
+
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1066,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
+
+console.log(fixDeath(people));
+// findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
