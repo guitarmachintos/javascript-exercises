938658c1167a1fc9acfea74ec5b28cfc2b9757db
guitarmachintos <guitarmachintos@gmail.com>
2026-01-15 13:19:17 +0700

    Finish ex8-removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removers) {
+    let newArr = arr.slice();
+    for (const remover of removers) {
+        let removedIndex = newArr.indexOf(remover);
+        while(removedIndex !== -1){
+            newArr.splice(removedIndex, 1);
+            removedIndex = newArr.indexOf(remover);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
